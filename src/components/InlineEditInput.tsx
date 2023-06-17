9a0a5bc1ecbd80cf66b8b1dfb95159f3031657f3
Mohit Shende <mohit@tartanhq.com>
2023-06-17 22:52:01 +0530

    created inlineInput component

--- src/components/InlineEditInput.tsx
+++ src/components/InlineEditInput.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import { useMutation } from "@apollo/client";
+import { useState } from "react";
 
-const InlineEditInput = () => {
-  return <div>InlineEditInput</div>;
+const InlineEditInput = ({
+  id,
+  field,
+  setShowEditInput,
+}: {
+  id: string;
+  field: string;
+  setShowEditInput: ({
+    fieldName,
+    show,
+  }: {
+    fieldName: string;
+    show: boolean;
+  }) => void;
+}) => {
+  const [payload, setPayload] = useState(null);
+
+  // const [patchLocation, { data, loading }] = useMutation(PATCH_LOCATION);
+
+  const handleChange = (e) => {
+    console.log(e);
+    setPayload({ ...payload, [e.target.name]: e.target.value });
+  };
+
+  if (field == "status") {
+    <select
+      name="status"
+      placeholder="Select Status"
+      value={payload?.status}
+      onChange={handleChange}
+    >
+      <option value="Active">Active</option>
+      <option value="Inactive">Inactive</option>
+    </select>;
+  }
+  return (
+    <input
+      name={field}
+      type="text"
+      placeholder={`Enter ${field} name`}
+      onChange={handleChange}
+    />
+  );
 };
 
 export default InlineEditInput;
--- src/components/LocationDetailsCard.tsx
+++ src/components/LocationDetailsCard.tsx
@@ -1,21 +1,33 @@
 import { useEffect, useState } from "react";
 import { GET_LOCATION } from "../GraphQL/Queries";
-import { useQuery } from "@apollo/client";
+import { NetworkStatus, useMutation, useQuery } from "@apollo/client";
 import "../styles/LocationDetailsCard.css";
+import { DELETE_LOCATION } from "../GraphQL/Mutations";
+import InlineEditInput from "./InlineEditInput";
 
 const LocationDetailsCard = ({ locationId }: { locationId: string }) => {
   const tenant = import.meta.env.VITE_TENANT;
   const [locationData, setLocationData] = useState(null);
-  const [showEditInput, setShowEditInput] = useState(false);
+  const [showEditInput, setShowEditInput] = useState({
+    fieldName: "",
+    show: false,
+  });
 
   const { loading, data, refetch, networkStatus } = useQuery(GET_LOCATION, {
     variables: { locationReadId: locationId, tenant },
     notifyOnNetworkStatusChange: true,
   });
 
+  const [deleteLocation, { loading: isDeleting }] =
+    useMutation(DELETE_LOCATION);
+
   useEffect(() => {
     if (data) {
       setLocationData(data.locationRead);
+      setShowEditInput({
+        fieldName: "",
+        show: false,
+      });
     }
   }, [data]);
 
@@ -29,7 +41,7 @@ const LocationDetailsCard = ({ locationId }: { locationId: string }) => {
 
   return (
     <div className="location-container">
-      {loading ? (
+      {networkStatus === NetworkStatus.refetch || loading ? (
         <p style={{ textAlign: "center" }}>Loading...</p>
       ) : (
         <>
@@ -44,19 +56,111 @@ const LocationDetailsCard = ({ locationId }: { locationId: string }) => {
 
             <button
               className="location-details-refresh"
-              onClick={() => refetch()}
+              onClick={() =>
+                deleteLocation({
+                  variables: {
+                    locationRemoveId: locationData?.id,
+                    tenant: import.meta.env.VITE_TENANT,
+                  },
+                })
+              }
+              disabled={isDeleting}
             >
               Delete
             </button>
           </div>
           <div className="location-details">
-            <div>
-              <p>{locationData?.resource?.address}</p>
-              <button onClick={() => setShowEditInput(true)}>Edit</button>
+            <div className="edit-detail">
+              <p>Address: {locationData?.resource?.address}</p>
+              {showEditInput.fieldName == "address" ? (
+                <InlineEditInput
+                  field={showEditInput.fieldName}
+                  setShowEditInput={setShowEditInput}
+                  id={locationId}
+                />
+              ) : (
+                <button
+                  onClick={() =>
+                    setShowEditInput({
+                      ...showEditInput,
+                      fieldName: "address",
+                      show: true,
+                    })
+                  }
+                >
+                  Edit
+                </button>
+              )}
             </div>
-            <p>{locationData?.resource?.npi}</p>
-            <p>{locationData?.resource?.status}</p>
-            <p>{locationData?.resource?.taxId}</p>
+
+            <div className="edit-detail">
+              <p>NPI: {locationData?.resource?.npi}</p>
+              {showEditInput.fieldName == "npi" ? (
+                <InlineEditInput
+                  field={showEditInput.fieldName}
+                  setShowEditInput={setShowEditInput}
+                  id={locationId}
+                />
+              ) : (
+                <button
+                  onClick={() =>
+                    setShowEditInput({
+                      ...showEditInput,
+                      fieldName: "npi",
+                      show: true,
+                    })
+                  }
+                >
+                  Edit
+                </button>
+              )}
+            </div>
+
+            <div className="edit-detail">
+              <p>Status: {locationData?.resource?.status}</p>
+              {showEditInput.fieldName == "status" ? (
+                <InlineEditInput
+                  field={showEditInput.fieldName}
+                  setShowEditInput={setShowEditInput}
+                  id={locationId}
+                />
+              ) : (
+                <button
+                  onClick={() =>
+                    setShowEditInput({
+                      ...showEditInput,
+                      fieldName: "status",
+                      show: true,
+                    })
+                  }
+                >
+                  Edit
+                </button>
+              )}
+            </div>
+
+            <div className="edit-detail">
+              <p>TaxId: {locationData?.resource?.taxId}</p>
+              {showEditInput.fieldName == "taxId" ? (
+                <InlineEditInput
+                  field={showEditInput.fieldName}
+                  setShowEditInput={setShowEditInput}
+                  id={locationId}
+                />
+              ) : (
+                <button
+                  onClick={() =>
+                    setShowEditInput({
+                      ...showEditInput,
+                      fieldName: "taxId",
+                      show: true,
+                    })
+                  }
+                >
+                  Edit
+                </button>
+              )}
+            </div>
           </div>
         </>
       )}
