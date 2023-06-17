6ba27c6937c30869ac43ad6deeb27b85efc2fce4
Mohit Shende <mohit@tartanhq.com>
2023-06-17 18:06:14 +0530

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://graphqlzero.almansi.me/api",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
